459dcd8db1df6dbb81a75dafe3cb847b24586df1
giuliavf <32200205+giuliavf@users.noreply.github.com>
2017-10-10 22:00:44 -0300

    "Adicionando exercícios"

---
+++ lista02/5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fatorial (int x){
+    int i;
+    for(i=x-1;i>0;i--){
+        x = x * i;
+    }
+}
+
+int coefbin (int n, int k){
+    int p1 = fatorial (n);
+    int p2 = fatorial (k);
+    int p3 = fatorial (n-k);
+
+    int resp = p1/(p2*p3);
+    return resp;
+}
+
+main(){
+    int n,k;
+
+    scanf("%d",&n);
+    scanf("%d",&k);
+
+    int t = coefbin (n,k);
+
+    printf("%d\n",t);       
+}
