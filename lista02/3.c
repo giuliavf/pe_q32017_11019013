8e64a566bb7b6573ad3320a8b2e50f9037178ba1
Giulia <giuliavf@hotmail.com>
2017-11-09 22:14:13 -0200

    add ex3

---
+++ lista02/3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+double gauss_legendre()
+{
+    double a = 1.0, b = 1.0/sqrt(2), t = 1.0/4.0, p = 1.0,a1,PI,n,m;
+    int i;
+
+    for (i = 0;i < 10; i++){
+        a1 = (a+b)/2.0;
+        b = sqrt (a*b);
+        n = (a - a1);
+        m = pow(n,2);
+        t = t - p*m;
+        p = 2.0*p;
+        a = a1;
+    }
+
+    double k = (a+b), j = pow (k,2);
+
+    return j/(4.0*t);
+
+}
+
+int main ()
+{   
+    printf ("%0.20lf\n",gauss_legendre());  
+    return 0;  
+}
